f6754c02602c1241e0e3a32b1ec17df235231e3b
Simske <mail@simske.com>
2021-05-01 18:26:14 +0200

    Implement basic lichess downloading

--- pgn_downloader/__main__.py
+++ pgn_downloader/__main__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 
-from . import chess_com
+from . import chess_com, lichess
 from .version import version
 
 
@@ -9,7 +9,21 @@ def cli_arguments(args=None):
     """Parse CLI arguments"""
     parser = argparse.ArgumentParser()
     parser.add_argument("--version", action="version", version=f"%(prog)s {version}")
-    parser.add_argument("username")
+    parser.add_argument(
+        "server", choices=("chess.com", "lichess"), help="Server to download games from"
+    )
+    # TODO: time format
+    parser.add_argument("username", help="Username on chosen server")
+    parser.add_argument(
+        "--since", help="Download games since (default: account creation)"
+    )
+    parser.add_argument("--until", help="Download games until (default: now)")
+    parser.add_argument(
+        "--mode",
+        default="blitz,rapid,classical,correspondence",
+        help="Game mode which will be downloaded. Comma separated list "
+        "(default: blitz,rapid,classical,correspondence)",
+    )
     parser.add_argument("-o", "--output", default=None, help="Output filename")
     parser.add_argument(
         "-c",
@@ -23,21 +37,33 @@ def cli_arguments(args=None):
     # generate output filename default
     if args.output is None:
         if args.color is None:
-            args.output = f"{args.username}.pgn"
+            args.output = f"{args.server}_{args.username}.pgn"
         else:
-            args.output = f"{args.username}-{args.color}.pgn"
+            args.output = f"{args.server}_{args.username}-{args.color}.pgn"
+
+    # parse playing modes
+    args.mode = args.mode.split(",")
 
     return args
 
 
 def main():
     args = cli_arguments()
-    pgn = chess_com.download_pgn(args.username, args.color)
 
-    with open(args.output, "x") as f:
-        f.write(pgn)
+    if args.server == "chess.com":
+        print("Filters other than color are not implemented for chess.com")
+        pgn = chess_com.download_pgn(args.username, args.color)
 
-    print(f"Saved pgn to {args.output}")
+        with open(args.output, "x") as f:
+            f.write(pgn)
+
+        print(f"Saved pgn to {args.output}")
+    elif args.server == "lichess":
+        print("Not all filters are implemented yet!")
+        lichess.download_pgn(
+            args.username, args.output, color=args.color, modes=args.mode
+        )
+        print("Downloaded games from Lichess")
 
 
 if __name__ == "__main__":
---
+++ pgn_downloader/lichess.py
@@ -0,0 +1,35 @@
+"""all functions related to lichess"""
+
+from datetime import datetime
+
+import requests
+
+LICHESS_ENDPOINT = "https://lichess.org/api"
+
+
+def download_pgn(
+    username: str,
+    output_path,
+    color: str = None,
+    since: datetime = None,
+    until: datetime = None,
+    modes: list = None,
+):
+    """Download PGN from lichess with specified filters"""
+    url = f"{LICHESS_ENDPOINT}/games/user/{username}"
+    params = {}
+    if since is not None:
+        # timestamps in milliseconds
+        params["since"] = int(since.timestamp) * 1000
+    if until is not None:
+        params["until"] = int(until.timestamp) * 1000
+    if color is not None:
+        params["color"] = color
+    if modes is not None:
+        params["perfType"] = ",".join(modes)
+
+    with requests.get(url, params=params, stream=True) as r:
+        r.raise_for_status()
+        with open(output_path, "xb") as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
